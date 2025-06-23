d5677455582ab826e0f83e581a85fed28f0936c4
alexanderek <Alexander.ek@live.se>
2025-06-23 15:11:47 +0200

    added user & userrole

---
+++ FastighetProjekt.Models/Models/User/User.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace FastighetProjekt.Models.Models.User;
+
+public class User
+{
+    public int UserId { get; set; }
+
+    [Required]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required]
+    public string LastName { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    public string PasswordHash { get; set; } = string.Empty;
+
+    [Required]
+    public UserRole Role { get; set; } = UserRole.Member; // default till 'Member' t.ex.
+}
