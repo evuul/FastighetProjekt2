1ce4fb06cd721d18e60d47acd9c1363692d6bd34
alexanderek <Alexander.ek@live.se>
2025-07-02 09:36:13 +0200

    Added payment controller

--- FastighetProjekt.API/obj/Debug/net8.0/FastighetProjekt.API.AssemblyInfo.cs
+++ FastighetProjekt.API/obj/Debug/net8.0/FastighetProjekt.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FastighetProjekt.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7370b8978d54d794a952d5ae52d07cfc0b2263d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18c6bfa58879edc595e184afdc51939cdabe9f28")]
 [assembly: System.Reflection.AssemblyProductAttribute("FastighetProjekt.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FastighetProjekt.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
