0afea1567ad4cf1fdc3bd7fefa6b869ae4448a9c
alexanderek <Alexander.ek@live.se>
2025-07-02 10:02:00 +0200

    asd

--- FastighetProjekt.API/obj/Debug/net8.0/FastighetProjekt.API.AssemblyInfo.cs
+++ FastighetProjekt.API/obj/Debug/net8.0/FastighetProjekt.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FastighetProjekt.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18c6bfa58879edc595e184afdc51939cdabe9f28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b60f6bb42f044d4918127dbbac6fd6107a5ac72d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FastighetProjekt.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FastighetProjekt.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
